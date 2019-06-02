22fd5173f5286a985b3311ebbf83c782ac25d9bc
syed <ibrahimsyed624@gmail.com>
2019-06-02 19:56:33 +0530

    Parsing string in java

---
+++ src/com/syed/day3/StringParse.java
@@ -0,0 +1,9 @@
+package com.syed.day3;
+
+public class StringParse {
+    public static void main(String[] args) {
+        String number = "100";
+        int value = Integer.parseInt(number) * 25;
+        System.out.println(value);
+    }
+}
