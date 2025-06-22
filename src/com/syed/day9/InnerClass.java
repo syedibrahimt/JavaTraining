e973aa2d68ecac5cb877ac0c385d37bf2d17e7e7
syedibrahimt <syedibrahim.developer@gmail.com>
2025-06-22 16:46:34 +0530

    with static inner class

--- src/com/syed/day9/InnerClass.java
+++ src/com/syed/day9/InnerClass.java
@@ -11,10 +11,22 @@ class A {
     }
 }
 
+class Vehicle {
+    public void drive(){
+        System.out.println("Drive");
+    }
+    static class Car {
+        public static void stop() {
+            System.out.println("Apply brakes");
+        }
+    }
+}
+
 public class InnerClass {
     public static void main(String[] args) {
         A a = new A();
         A.B b = a.new B();
         b.show();
+        Vehicle.Car.stop();
     }
 }
