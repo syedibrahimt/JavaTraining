1975b38c7ccbadead68dd8f7d878b2bc2fdda8e2
Syed Ibrahim <syedibrahim.developer@gmail.com>
2025-06-30 09:56:38 +0530

    Collections ArrayList

---
+++ src/com/syed/day14/CollectionsApi.java
@@ -0,0 +1,17 @@
+package com.syed.day14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionsApi {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<String>();
+        names.add("Syed");
+        names.add("Ibrahim");
+        names.add("Afrin");
+        names.add("Fatima");
+        names.add("Syed");
+        names.add("Hakeem");
+        System.out.println(names);
+    }
+}
